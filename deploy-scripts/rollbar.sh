4abb5f8cb65823049a5bac26a0bf838acb94f78f
David Dunlop <ddunlop@onemedical.com>
2019-11-15 15:24:11 -0800

    ..

--- deploy-scripts/rollbar.sh
+++ deploy-scripts/rollbar.sh
@@ -9,8 +9,10 @@ BASE_REPO=`realpath $BASEDIR/..`
 
 REVISION_FULLPATH=`realpath $BASE_REPO/$REVISION_LOCATION`
 
+cat $REVISION_FULLPATH
+
 # curl -i -X POST https://api.rollbar.com/api/1/deploy/ \
 curl -i -X POST https://postman-echo.com/post \
-  -F "access_token=ROLLBAR_ACCESS_TOKEN" \
+  -F "access_token=$ROLLBAR_ACCESS_TOKEN" \
   -F "environment=$ENVIRONMENT" \
   -F "revision="$(cat $REVISION_FULLPATH)
