20dd9c5c2a36c6866ec43058cdee9bb4ca2c65f3
David Dunlop <ddunlop@onemedical.com>
2019-11-15 13:27:56 -0800

    Is this the location

--- deploy-scripts/rollbar.sh
+++ deploy-scripts/rollbar.sh
@@ -6,6 +6,8 @@ echo "inside rollbar script"
 echo $@
 echo "0: $0"
 BASEDIR="`dirname $0`"
+BASE_REPO="`cd "$BASEDIR/.." && pwd`"
 
-SHAFILE="$BASEDIR/$1"
+SHAFILE="$BASE_REPO/$1"
 ls $SHAFILE
+cat $SHAFILE
