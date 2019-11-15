c0a1e190e27a9dcfe01e2bd258ffb7d1ff01d1ad
David Dunlop <ddunlop@onemedical.com>
2019-11-15 11:13:04 -0800

    initial commit

---
+++ deploy-scripts/git_metadata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+OUTDIR="$1"
+
+BASEDIR="`dirname $0`"
+
+BASE_REPO="`cd "$BASEDIR/../.." && pwd`"
+
+cd "$BASE_REPO"
+
+mkdir -p "$OUTDIR"
+git rev-parse HEAD > "$OUTDIR/SHA"
