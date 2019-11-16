c46021a2f36dd3675185b566e20d7c88a666968c
David Dunlop <ddunlop@onemedical.com>
2019-11-15 16:29:57 -0800

    ..

--- deploy-scripts/rollbar.sh
+++ deploy-scripts/rollbar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 REVISION_FILE=$1
 
@@ -9,7 +9,11 @@ REPO_BASE_DIR=`realpath $SCRIPT_DIR/..`
 
 REVISION_FULLPATH=`realpath $REPO_BASE_DIR/$REVISION_FILE`
 
+SHA=$(cat $REVISION_FULLPATH)
+
+echo "Updating the $ENVIRONMENT to revision: $SHA"
+
 curl -i -X POST https://api.rollbar.com/api/1/deploy/ \
   -F "access_token=$ROLLBAR_ACCESS_TOKEN" \
   -F "environment=$ENVIRONMENT" \
-  -F "revision="$(cat $REVISION_FULLPATH)
+  -F "revision=$SHA"
